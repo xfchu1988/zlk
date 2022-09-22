f80acc6428ec72f2de532f99357501944647f855
xfchu1988 <86211876+xfchu1988@users.noreply.github.com>
2022-09-22 17:14:14 +0800

    Add files via upload

--- utils/JD_DailyBonus.js
+++ utils/JD_DailyBonus.js
@@ -16,7 +16,7 @@ var Key = ''; //该参数已废弃; 仅用于下游脚本的兼容, 请使用jso
 
 var DualKey = ''; //该参数已废弃; 仅用于下游脚本的兼容, 请使用json串数据  ↓
 
-var OtherKey = ``; //无限账号Cookie json串数据, 请严格按照json格式填写, 具体格式请看以下样例:
+var OtherKey = `[{"cookie":"pt_key=AAJjJXMBADANas1yTeoAS7yS1DbCkYY7mIFfKWJ2iH8CDbzGxtqFqKeeHL-JOW-634vf4SvJcJg;pt_pin=jd_66fc783c77a43;","jrBody":""}]`; //无限账号Cookie json串数据, 请严格按照json格式填写, 具体格式请看以下样例:
 
 
 var LogDetails = false; //是否开启响应日志, true则开启
